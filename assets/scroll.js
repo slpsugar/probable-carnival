23d2ce67119669ccf63caa94c7e2484018051880
slpsugar <poppadomsorbread@gmail.com>
2021-12-28 11:54:04 +0300

    photo update

--- assets/scroll.js
+++ assets/scroll.js
@@ -1,155 +1,126 @@
-// web
-var luchinin = document.querySelector (".luchinin");
-luchinin.addEventListener("click", function(){
-    var preview = document.getElementById("luchinin");
-    preview.scrollIntoView({
+const buttonUp = document.querySelector(".top");
+
+const luchinin = document.querySelector(".luchinin");
+
+const wallace = document.querySelector(".wallace");
+const denial = document.querySelector(".denial");
+const bin = document.querySelector(".bin");
+const maze = document.querySelector(".maze");
+const hopscotch = document.querySelector(".hopscotch");
+const cart = document.querySelector(".cart");
+const choiceOfThree = document.querySelector(".choice");
+const moomin = document.querySelector(".moomin");
+const stairs = document.querySelector(".stairs");
+const dive = document.querySelector(".dive");
+const tlsp = document.querySelector(".tlsp");
+const office = document.querySelector(".office");
+
+const marks = document.querySelector (".marks");
+const faces = document.querySelector(".faces");
+const street = document.querySelector(".street");
+
+//scroll
+const showPreviewTop = (id) => {
+    const element = document.getElementById(id);
+    element.scrollIntoView({
         behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+    element.classList.add("show-lg-3");
+};
+
+const showPreviewCentre = (id) => {
+    const element = document.getElementById(id);
+    element.scrollIntoView({
+        block:"center",
+        behavior:"smooth"});
+    element.classList.add("show-lg-3");
+};
+
+// web
+luchinin.addEventListener("click", function () {
+    showPreviewTop("luchinin");
 });
 
 // illustration
-var wallace = document.querySelector (".wallace");
-wallace.addEventListener("click", function(){
-    var preview = document.getElementById("wallace");
-    preview.scrollIntoView({
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+wallace.addEventListener("click", function () {
+    showPreviewTop("wallace");
 });
 
-var denial = document.querySelector (".denial");
 denial.addEventListener("click", function(){
-    var preview = document.getElementById("denial");
-    preview.scrollIntoView({
-        block:"center",
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+    showPreviewCentre("denial");
 });
 
-var bin = document.querySelector (".bin");
 bin.addEventListener("click", function(){
-    var preview = document.getElementById("bin");
-    preview.scrollIntoView({
-        block:"center",
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+    showPreviewCentre("bin");
 });
 
-var maze = document.querySelector (".maze");
 maze.addEventListener("click", function(){
-    var preview = document.getElementById("maze");
-    preview.scrollIntoView({
-        block:"center",
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+    showPreviewCentre("maze");
 });
 
-document.querySelector(".hopscotch").addEventListener("click", function () {
-    var preview = document.getElementById("hopscotch");
-    preview.scrollIntoView({
-        block:"center",
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+hopscotch.addEventListener("click", function () {
+    showPreviewCentre("hopscotch");
 });
 
-document.querySelector(".cart").addEventListener("click", function () {
-    var preview = document.getElementById("cart");
-    preview.scrollIntoView({
-        block:"center",
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+cart.addEventListener("click", function () {
+    showPreviewCentre("cart");
 });
 
-document.querySelector(".choice").addEventListener("click", function () {
-    var preview = document.getElementById("choice");
-    preview.scrollIntoView({
-        block:"center",
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+choiceOfThree.addEventListener("click", function () {
+    showPreviewCentre("choice");
 });
 
-document.querySelector(".moomin").addEventListener("click", function () {
-    var preview = document.getElementById("moomin");
-    preview.scrollIntoView({
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+moomin.addEventListener("click", function () {
+    showPreviewCentre("moomin");
 });
 
-document.querySelector(".stairs").addEventListener("click", function () {
-    var preview = document.getElementById("stairs");
-    preview.scrollIntoView({
-        block:"center",
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+stairs.addEventListener("click", function () {
+    showPreviewCentre("stairs");
 });
 
-document.querySelector(".dive").addEventListener("click", function () {
-    var preview = document.getElementById("dive");
-    preview.scrollIntoView({
-        block:"center",
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+dive.addEventListener("click", function () {
+    showPreviewCentre("dive");
 });
 
-document.querySelector(".tlsp").addEventListener("click", function () {
-    var preview = document.getElementById("tlsp");
-    preview.scrollIntoView({
-        block:"center",
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+tlsp.addEventListener("click", function () {
+    showPreviewCentre("tlsp");
 });
 
-document.querySelector(".office").addEventListener("click", function () {
-    var preview = document.getElementById("office");
-    preview.scrollIntoView({
-        block:"center",
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+office.addEventListener("click", function () {
+    showPreviewCentre("office");
 });
 
 
 
 // photography
-var marks = document.querySelector (".marks");
 marks.addEventListener("click", function(){
-    var preview = document.getElementById("marks");
-    preview.scrollIntoView({
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+    showPreviewTop("marks");
 });
 
-
-document.querySelector(".faces").addEventListener("click", function () {
-    var preview = document.getElementById("faces");
-    preview.scrollIntoView({
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+faces.addEventListener("click", function () {
+    showPreviewTop("faces");
 });
 
-document.querySelector(".street").addEventListener("click", function () {
-    var preview = document.getElementById("street");
-    preview.scrollIntoView({
-        behavior:"smooth"});
-    preview.classList.add("show-lg-3");
+street.addEventListener("click", function () {
+    showPreviewTop("street");
 });
 
 
 
 // IntersectionObserver
 const faders = document.querySelectorAll(".hidden-lg-3");
-
 const appearOptions = {
     threshold: 0,
     rootMargin: "0px 0px -400px 0px"
 };
-
 const appearOnscroll = new IntersectionObserver
-(function (
-    entries,
-    appearOnscroll) {
+(function (entries, appearOnscroll) {
         entries.forEach(entry => {
-            if (!entry.isIntersecting) {return;} else {entry.target.classList.add('appear');
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+            entry.target.classList.add('appear');
             appearOnscroll.unobserve (entry.target);
-        }
+            }
         });
     },
 appearOptions);
@@ -157,7 +128,7 @@ appearOptions);
 faders.forEach(fader => {appearOnscroll.observe(fader);});
 
 // MEDIA QUERIES
-document.querySelector(".top").addEventListener("click", function () {
+buttonUp.addEventListener("click", function () {
     const main = document.getElementById("main-content");
     main.scrollIntoView({
         behavior:"smooth"});
